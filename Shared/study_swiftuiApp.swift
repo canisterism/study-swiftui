88df2b05ad61d9c81ea968b65c7ab9cdade3bfa7
canisterism <ohtthstc@gmail.com>
2022-01-25 15:12:54 +0900

    Initial Commit

---
+++ Shared/study_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  study_swiftuiApp.swift
+//  Shared
+//
+//  Created by canisterism on 2022/01/25.
+//
+
+import SwiftUI
+
+@main
+struct study_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
